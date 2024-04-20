56241caafd6069331aabd2e39e3f0cc23ab5e823
Ace Mushroom <mt813453@dal.ca>
2024-04-19 21:11:33 -0300

    Add Mongoose dependency

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import express from "express";
+import express,{ Request, Response, NextFunction } from 'express';
 
 const app = express();
 const port = 3000;
+
+
+app.get("/", (req: any, res: any) => {
+  res.send("<h1>Hi from my project</h1>")
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
